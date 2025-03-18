1de06f77d8a0503f3590dc6b66722f6723674e00
Bint <2393714045@qq.com>
2025-03-18 18:57:21 +0800

    feat: 隐藏大屏

--- vue-vben-admin/apps/web-ele/src/router/routes/modules/monitor.ts
+++ vue-vben-admin/apps/web-ele/src/router/routes/modules/monitor.ts
@@ -1,17 +1,17 @@
-import type { RouteRecordRaw } from 'vue-router';
+// import type { RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/monitor',
-    name: 'Monitor',
-    component: () => import('#/views/equipment/statistics/index.vue'),
-    meta: {
-      title: '大田监控',
-      icon: 'ep:monitor',
-      // hideInMenu: true,
-      noBasicLayout: true,
-    },
-  },
-];
+// const routes: RouteRecordRaw[] = [
+//   {
+//     path: '/monitor',
+//     name: 'Monitor',
+//     component: () => import('#/views/equipment/statistics/index.vue'),
+//     meta: {
+//       title: '大田监控',
+//       icon: 'ep:monitor',
+//       // hideInMenu: true,
+//       noBasicLayout: true,
+//     },
+//   },
+// ];
 
-export default routes;
+// export default routes;
